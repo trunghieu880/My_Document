3161743ab1d75049f9c5c74c93763aabfdec8909
hieunguyen <hieu.nguyen-trung@banvien.com.vn>
2020-05-15 10:13:01 +0700

    Update

---
+++ 999_NW/rbvh/parse_Test_CTR/script.sh
@@ -0,0 +1,5 @@
+sed -i 's@START_TEST("1: rba_BldrCmp_RC_EraseMemory",@START_TEST("100: rba_BldrCmp_RC_EraseMemory",@g' 'C:/Users/hieu.nguyen-trung/Desktop/Test_Folder/a.c'
+sed -i 's@START_TEST("2: rba_BldrCmp_RC_EraseMemory",@START_TEST("101: rba_BldrCmp_RC_EraseMemory",@g' 'C:/Users/hieu.nguyen-trung/Desktop/Test_Folder/a.c'
+sed -i 's@START_TEST("3: rba_BldrCmp_RC_EraseMemory",@START_TEST("102: rba_BldrCmp_RC_EraseMemory",@g' 'C:/Users/hieu.nguyen-trung/Desktop/Test_Folder/a.c'
+sed -i 's@START_TEST("4: rba_BldrCmp_RC_EraseMemory",@START_TEST("103: rba_BldrCmp_RC_EraseMemory",@g' 'C:/Users/hieu.nguyen-trung/Desktop/Test_Folder/a.c'
+sed -i 's@START_TEST("5: rba_BldrCmp_RC_EraseMemory",@START_TEST("104: rba_BldrCmp_RC_EraseMemory",@g' 'C:/Users/hieu.nguyen-trung/Desktop/Test_Folder/a.c'
